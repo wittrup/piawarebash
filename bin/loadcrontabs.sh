f10b65abcded9fbf183ff0dc5e4a0bb699c34771
wittrup <wittrup@gmail.com>
2017-11-16 09:17:15 +0000

    Must remember comment for script name...nclp???

--- bin/loadcrontabs.sh
+++ bin/loadcrontabs.sh
@@ -1,1 +1,2 @@
-sudo tail /var/spool/cron/crontabs/$USER > "${HOME}/${USER}crontabs"
+# https://stackoverflow.com/questions/7103531/how-to-get-the-part-of-file-after-the-line-that-matches-grep-expression-first/7104422#7104422
+sudo cat /var/spool/cron/crontabs/$USER | sed -n -e '/# m h  dom mon dow   command/,$p' > "${HOME}/${USER}crontabs"
